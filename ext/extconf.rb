01f83f4f69a44eb6e0c9c4dfc0f481a9b314f311
uriasn <urias@gnuif.nl>
2018-06-07 16:22:01 +0200

    Vendored R

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,7 +3,7 @@ require 'mkmf'
 dir_config('R')
 
 some_paths = ENV['PATH'].split(File::PATH_SEPARATOR) + %w[
-  /app/.root/usr/lib/R
+  /app/vendor/R
   /usr/local/lib64/R
   /usr/local/lib/R 
   /usr/lib64/R 
